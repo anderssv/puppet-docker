2dcfc47937be30c76d8a45e4ac50a78c42a61f44
Anders Sveen <anders@f12.no>
2013-08-21 21:57:18 +0200

    Testing librarian

--- docker/run_puppet.sh
+++ docker/run_puppet.sh
@@ -2,4 +2,11 @@
 
 cd /puppet
 
+# Warning this part is highly experiemental, might not work. Sorry, later. :)
+if [[ -e ./Puppetfile ]]; then
+	echo "Running librarian to handle module dependencies..."
+	librarian-puppet install
+fi
+
+echo "Applying puppet..."
 puppet apply --modulepath modules manifests/local.pp --no-usecacheonfailure --verbose
--- docker/system_packages.sh
+++ docker/system_packages.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Should this have -eu ? Gives weird behaviour?
 
 echo "Updating system and adding required packages..."
 
 echo "sslverify=false" >> /etc/yum.conf
 yum update -y
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install puppet git nano openssh-server passwd -y
+yum install puppet git nano openssh-server passwd rubygems ruby-devel make gcc -y
+
+gem install librarian-puppet
